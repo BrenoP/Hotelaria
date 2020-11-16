d401724369546b393508dd22d1a435e2f7e227b0
Breno Peretta <peretta.breno@gmail.com>
2020-11-16 19:17:12 -0300

    Updates

---
+++ precache-manifest.069de2f70592e9e9c0a975a80a409989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadce003018fa84cb3043f5623be4d92",
+    "url": "/Hotelaria/index.html"
+  },
+  {
+    "revision": "f4b1150767d077cffa6e",
+    "url": "/Hotelaria/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "545c3f6b651a1f3b596b",
+    "url": "/Hotelaria/static/css/main.d3fb6868.chunk.css"
+  },
+  {
+    "revision": "f4b1150767d077cffa6e",
+    "url": "/Hotelaria/static/js/2.1157b1e8.chunk.js"
+  },
+  {
+    "revision": "92030348a26ced4244158f5af7d18a19",
+    "url": "/Hotelaria/static/js/2.1157b1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545c3f6b651a1f3b596b",
+    "url": "/Hotelaria/static/js/main.8f144d0e.chunk.js"
+  },
+  {
+    "revision": "e9623cef25cc2ec3df88",
+    "url": "/Hotelaria/static/js/runtime-main.b5caa70d.js"
+  }
+]);
