6fa43141a5cf937d2287d2011442e12211bfa4d0
Breno Peretta <peretta.breno@gmail.com>
2020-11-26 22:59:12 -0300

    Script do hotjar

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,65 +1,63 @@
-import React, { useState } from "react";
-import TextField from "@material-ui/core/TextField";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 
-import {
-   LoginContainer, 
-   FormContainer,
-} from "./styles";
+import { LoginContainer, FormContainer } from './styles';
 
-import api from "~/services/api";
+import api from '~/services/api';
 
 export default function Login() {
-   const dispatch = useDispatch();
-   const history = useHistory();
-   const [password, setPassword] = useState("");
-   const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
-   function handleLogin() {
-      let formData = new FormData();
-      formData.append('mail', email);
-      formData.append('password', password);
+  useEffect(() => {
+    console.log('Script');
+  }, []);
 
-      api.post("/funcionario/validateFunc.php", formData)
-      .then((res) => {
-         dispatch({ type: "SET_USER", user: JSON.parse(res.data.slice(176)) })
-         toast.success("Logado com sucesso");
-         history.push("/inicio");
-      }) 
-      .catch((err) => {
-         toast.error("Ocorreu algum erro, tente novamente!");
+  function handleLogin() {
+    let formData = new FormData();
+    formData.append('mail', email);
+    formData.append('password', password);
+
+    api
+      .post('/funcionario/validateFunc.php', formData)
+      .then(res => {
+        dispatch({ type: 'SET_USER', user: JSON.parse(res.data.slice(176)) });
+        toast.success('Logado com sucesso');
+        history.push('/inicio');
       })
-   }
+      .catch(err => {
+        toast.error('Ocorreu algum erro, tente novamente!');
+      });
+  }
 
-   return (
-      <LoginContainer>
-         <FormContainer>
-            <h3>
-               Login
-            </h3>
-            <TextField
-               required
-               id="outlined-required"
-               label="Email"
-               variant="outlined"
-               style={{ marginBottom: "1.5rem" }}
-               onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-               required
-               id="outlined-password-input"
-               label="Senha"
-               type="password"
-               autoComplete="current-password"
-               variant="outlined"
-               onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={() => handleLogin()}>
-               Entrar
-            </button>
-         </FormContainer>
-      </LoginContainer>
-   );
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <h3>Login</h3>
+        <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          variant="outlined"
+          style={{ marginBottom: '1.5rem' }}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-password-input"
+          label="Senha"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button onClick={() => handleLogin()}>Entrar</button>
+      </FormContainer>
+    </LoginContainer>
+  );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "~/pages/Auth/Login";
 import Home from "~/pages/Home";
